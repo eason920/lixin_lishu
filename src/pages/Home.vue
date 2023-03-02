faaca26506067a3389da4b4acc97d65279408511
eason920 <eason920@gmail.com>
2023-03-02 23:35:26 +0800

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="home no-padding-top">
+    <Loading :loading="load" />
+    <SideNavigation v-if="isSide" />
+    <Navigation v-if="!isMobile" />
+    <MClose v-else />
+    <!-- <Indigator :viewIndex="viewIndex" /> -->
+    <!-- <full-page
+      ref="fullPage"
+      :options="options"
+      id="fullpage"
+    > -->
+    <!-- <vue-lazy-component class="section" id="sectionDe" @init="init">
+      <SwiperDemo />
+    </vue-lazy-component> -->
+    <vue-lazy-component class="section" id="section1" @init="init">
+      <S1 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section2" @init="init">
+      <S2 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section0" @init="init">
+      <S0 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section3" @init="init">
+      <S3 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section4" @init="init">
+      <S4 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section5" @init="init">
+      <S5 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section6" @init="init">
+      <S6 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section7" @init="init">
+      <S7 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="section8" @init="init">
+      <S8 />
+    </vue-lazy-component>
+    <vue-lazy-component class="section" id="contact">
+      <ContactSection />
+    </vue-lazy-component>
+    <!-- ======================== -->
+    <MobileNav />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import $ from 'jquery'
+import Navigation from '@/layouts/Navigation.vue'
+import { isMobile } from '@/utils'
+import SideNavigation from '@/layouts/SideNavigation.vue'
+import ContactSection from '@/layouts/ContactSection.vue'
+import MobileNav from '@/layouts/MobileNav.vue'
+import Loading from '@/components/Loading.vue'
+// import Indigator from '@/components/Indigator.vue'
+// import SwiperDemo from '@/projects/lishu/swiperDemo_S7_single.vue'
+import MClose from '@/projects/lishu/MClose.vue'
+import S0 from '@/projects/lishu/S0.vue'
+import S1 from '@/projects/lishu/S1.vue'
+import S2 from '@/projects/lishu/S2.vue'
+import S3 from '@/projects/lishu/S3.vue'
+import S4 from '@/projects/lishu/S4.vue'
+import S5 from '@/projects/lishu/S5.vue'
+import S6 from '@/projects/lishu/S6.vue'
+import S7 from '@/projects/lishu/S7.vue'
+import S8 from '@/projects/lishu/S8.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Loading,
+    // Indigator,
+    Navigation,
+    SideNavigation,
+    ContactSection,
+    MobileNav,
+    // SwiperDemo,
+    MClose,
+    S0,
+    S1,
+    S2,
+    S3,
+    S4,
+    S5,
+    S6,
+    S7,
+    S8,
+  },
+
+  data() {
+    return {
+      isMobile,
+      isSide: false,
+      load: true,
+      pcTop1: '',
+      pcTop2: '',
+      pcTop3: '',
+      pcTop4: '',
+      //
+      mbTop1: '',
+      mbTop2: '',
+      mbTop3: '',
+      mbTop4: '',
+      mbTop5: '',
+      mbTop6: '',
+    }
+  },
+  created() {
+    $(document).ready(() => {
+      // Images loaded is zero because we're going to process a new set of images.
+      var imagesLoaded = 0
+      // Total images is still the total number of <img> elements on the page.
+      var totalImages = $('img').length
+
+      const allImagesLoaded = () => {
+        this.load = false
+      }
+      const imageLoaded = () => {
+        imagesLoaded++
+        if (imagesLoaded === totalImages) {
+          allImagesLoaded()
+        }
+      }
+      $('img').each(function(idx, img) {
+        $('<img>')
+          .on('load', imageLoaded)
+          .attr('src', $(img).attr('src'))
+      })
+
+      // const fnPcTop1 = () => {
+      //   const h = document.getElementById('section1').offsetHeight
+      //   const st = $(window).scrollTop() + $(window).height()
+      //   const top = 'top:' + (h + st / 100 * -15) + 'px'
+      //   this.pcTop1 = top
+      // }
+    })
+  },
+  mounted() {
+
+  },
+  methods: {
+    init() {}
+  }
+}
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Noto+Serif+TC')
+@import "src/assets/style/myvar"
+*
+  font-family: "Noto Serif TC", serif !important
+  letter-spacing: 1px
+  // Noto Sans CJK TC  粗細Regular 400
+  // Noto Serif CJK TC  粗細Regular 400  Bold 700
+section
+  // overflow: visible
+  position: relative
+
+@media screen and (min-width: $bp-pc)
+
+@media screen and (max-width: $bp-mb)
+
+</style>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Playball&display=swap');
+@import '../assets/style/variableColor.scss';
+
+.section,
+.section .fp-slide,
+.section .fp-tableCell {
+  height: auto !important;
+}
+
+</style>
